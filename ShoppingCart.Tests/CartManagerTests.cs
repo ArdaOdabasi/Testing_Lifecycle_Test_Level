c4600534c6cbb62a115a6de1e34c6c90fe3a7f54
ArdaOdabasi <61662021+ArdaOdabasi@users.noreply.github.com>
2022-07-07 15:42:53 +0300

    Successfully Completed
    The project was completed successfully.

---
+++ ShoppingCart.Tests/CartManagerTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Tests
+{
+    [TestClass]
+    public class CartManagerTests
+    {
+        private CartItem _cartItem;
+        private CartManager _cartManager;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _cartManager = new CartManager();
+            _cartItem = new CartItem
+            {
+                Product = new Product
+                {
+                    ProductId = 6,
+                    ProductName = "Telefon",
+                    UnitPrice = 6000
+                },
+                Quantity = 1
+            };
+    
+            _cartManager.Add(_cartItem);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _cartManager.Clear();
+        }
+                   
+        [TestMethod]
+        public void Sepete_urun_eklenebilmelidir()
+        {
+            //Arrange
+            const int beklenen = 1;
+
+            //Act
+            var toplamElemanSayisi = _cartManager.TotalItems;
+
+            //Assert
+            Assert.AreEqual(beklenen, toplamElemanSayisi);
+        }
+
+        [TestMethod]
+        public void Sepette_olan_urun_cikarilabilmelidir()
+        {
+            //Arrange          
+            var sepetteOlanElemanSayisi = _cartManager.TotalItems;
+
+            //Act
+            _cartManager.Remove(6);
+            var sepetteKalanElemanSayisi = _cartManager.TotalItems;
+
+            //Assert
+            Assert.AreEqual(sepetteOlanElemanSayisi - 1, sepetteKalanElemanSayisi);
+        }
+
+        [TestMethod]
+        public void Sepet_temizlenebilmelidir()
+        {         
+            //Act
+            _cartManager.Clear();
+
+            //Assert
+            Assert.AreEqual(0, _cartManager.TotalQuantity);
+            Assert.AreEqual(0, _cartManager.TotalItems);
+        }
+    }
+}
---
+++ ShoppingCart.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShoppingCart.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShoppingCart.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5028e8d1-a6b0-4bd7-bda2-952520ea14e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
